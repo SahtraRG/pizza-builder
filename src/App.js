a51512f71677ec7016be4c580c5817ee527c077f
SahtraRG <sahtra01@gmail.com>
2023-03-15 13:37:51 -0600

    add drag and drop logic for toppings

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import {pizza, toppings} from './pizzaObjects';
 
 function App() {
+
+  const [toppingBeingDragged, setToppingBeingDragged] = useState([])
+
+
+  const handleOnDrag = (e, toppingType) => {
+    e.dataTransfer.setData("text/plain", toppingType)
+  }
+
+  const handleOnDrop = (e) => {
+    const widgetType = e.dataTransfer.getData("text/plain")
+    setToppingBeingDragged([...toppingBeingDragged, widgetType])
+  }
+
+  const handleDragOver = (e) => {
+    e.preventDefault()
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='pizza' onDrop={handleOnDrop} onDragOver={handleDragOver}>
+      <img alt='pizza crust' src={pizza.crust} />
+      <img alt='pizza csauce' src={pizza.sauce} />
+      <img alt='pizza cheese' src={pizza.cheese} />
+      {
+        toppings.map(topping => {
+          if(toppingBeingDragged.includes(topping.id)){
+            return (
+              <img alt='topping' key={topping.id} src={topping.img} />
+            )
+          }
+        })
+      }
     </div>
+    <div className='topping-options'>
+      <div className='topping' draggable onDragStart={(e) => handleOnDrag(e, toppings[0].id)}>
+        Bell Pepper
+      </div>
+      <div className='topping' draggable onDragStart={(e) => handleOnDrag(e, toppings[1].id)}>
+        Mushroom
+      </div>
+      <div className='topping' draggable onDragStart={(e) => handleOnDrag(e, toppings[2].id)}>
+        Black Olive
+      </div>
+      <div className='topping' draggable onDragStart={(e) => handleOnDrag(e, toppings[3].id)}>
+        Red Onion
+      </div>
+      <div className='topping' draggable onDragStart={(e) => handleOnDrag(e, toppings[4].id)}>
+        Pepperoni
+      </div>
+    </div>
+    </>
   );
 }
 
---
+++ src/pizzaObjects.js
@@ -0,0 +1,32 @@
+export const pizza = {
+  crust: "pizza-art-big/crust.png",
+  sauce: "pizza-art-big/red-sauce.png",
+  cheese: "pizza-art-big/cheese.png",
+  bellPepper: "pizza-art-big/bell-pepper.png",
+  mushroom: "pizza-art-big/mushroom.png",
+  redOnion: "pizza-art-big/red-onion.png",
+  blackOlives: "pizza-art-big/black-olive.png",
+};
+
+export const toppings = [
+  {
+    id: "bell-peppers",
+    img: "pizza-art-big/bell-pepper.png",
+  },
+  {
+    id: 'mushroom',
+    img: "pizza-art-big/mushroom.png"
+  },
+  {
+    id: 'black-olive',
+    img: 'pizza-art-big/black-olive.png',
+  },
+  {
+    id: 'red-onion',
+    img: 'pizza-art-big/red-onion.png',
+  },
+  {
+    id: 'pepperoni',
+    img: 'pizza-art-big/pepperoni.png',
+  }
+]
